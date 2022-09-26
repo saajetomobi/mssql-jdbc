36ade697305e3266270b062f0d8b34cbe0607871
Jeff Wasty <v-jeffwasty@microsoft.com>
2022-09-26 13:21:29 -0700

    BigDecimal fixes that were confirmed working before I added additional 'cosmetic' changes.

--- src/main/java/com/microsoft/sqlserver/jdbc/Parameter.java
+++ src/main/java/com/microsoft/sqlserver/jdbc/Parameter.java
@@ -426,13 +426,15 @@ final class Parameter {
 
         private final Parameter param;
         private final SQLServerConnection con;
+        
+        private BigDecimal providedDecimal;
 
         GetTypeDefinitionOp(Parameter param, SQLServerConnection con) {
             this.param = param;
             this.con = con;
         }
 
-        private void setTypeDefinition(DTV dtv) {
+        private void setTypeDefinition(DTV dtv) throws SQLServerException {
             switch (dtv.getJdbcType()) {
                 case TINYINT:
                     param.typeDefinition = SSType.TINYINT.toString();
@@ -531,11 +533,16 @@ final class Parameter {
                         if (userProvidesPrecision) {
                             param.typeDefinition = "decimal(" + valueLength + "," + scale + ")";
                         }
-                    } else
+                    } else if (dtv.getJavaType() == JavaType.BIGDECIMAL
+                            && (providedDecimal = (BigDecimal) dtv.getValue(dtv.getJdbcType(), scale, null, null,
+                            typeInfo, cryptoMeta, null, null)) != null
+                        && providedDecimal.precision() >= scale) {
+                            param.typeDefinition = "decimal(" + providedDecimal.precision() + "," + scale + ")";
+                    } else {
                         param.typeDefinition = "decimal(" + SQLServerConnection.maxDecimalPrecision + "," + scale + ")";
+                    }
 
                     break;
-
                 case MONEY:
                     param.typeDefinition = SSType.MONEY.toString();
                     break;
